0c2e910864a6212d85946162ff1e59538c4d9890
Kirill <kkpetrov.dev@gmail.com>
2025-08-17 21:00:47 +0300

    Update biome config

--- apps/bot/src/main.ts
+++ apps/bot/src/main.ts
@@ -4,8 +4,6 @@ const TOKEN = Bun.env.BOT_TOKEN;
 
 const bot = new Bot(TOKEN);
 
-bot.on("message:text", (ctx) =>
-    ctx.reply(`Echo: ${ctx.message.text}`),
-);
+bot.on("message:text", (ctx) => ctx.reply(`Echo: ${ctx.message.text}`));
 
 bot.start();
